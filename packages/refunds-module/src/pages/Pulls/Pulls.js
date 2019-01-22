8518e07250b0c5fa269a67a267c8afc8e1139908
SerikITSTEP <aliarystan@gmail.com>
2019-01-22 21:28:28 +0600

    Pulls Files

--- packages/refunds-module/src/pages/Pulls/Pulls.js
+++ packages/refunds-module/src/pages/Pulls/Pulls.js
@@ -132,7 +132,62 @@ class Pulls extends Component {
             href="#"> <span><Badge
             status={this.setBadgeStatus(value.refund.isRefundConfirm)}/></span> {value.refund.dappRefundStatusId ? value.refund.dappRefundStatusId.nameRu : null}
           </a>
-        }],
+        },
+        {
+          title: "Загрузить",
+          order: 50,
+          key: "upload",
+          className: "action_column",
+          isVisible: true,
+          onCell: record => {
+            return {
+              onClick: () => {
+               // this.uploadFile(record.refund.id);
+                //console.log("upload file");
+                //console.log(record.refund.id);
+              }
+            };
+          },
+          render: (record) => (
+            <Upload
+              showUploadList={false}
+              openFileDialogOnClick={true}
+              onRemove={() => {}}
+              onPreview={() => {}}
+              beforeUpload={(file) => { return false;}}
+              onChange={(file) => {
+                if (file.status !== "removing") {
+                  this.uploadFile(record.refund.id, file);
+                }
+              }}>
+              <Icon type="database" theme="outlined"/>
+            </Upload>
+          )
+        },
+        {
+          title: "Файлы",
+          order: 51,
+          key: "files",
+          className: "action_column",
+          isVisible: true,
+          onCell: record => {
+            return {
+              onClick: () => {
+
+              }
+            };
+          },
+          render: (record) => (
+            <div>
+              {record.refund.refundFiles && record.refund.refundFiles.map((item) => {
+                return <p >{item.filename}  <a onClick={()=>{
+                  this.deleteFile(record, item);
+                }}>x</a></p>;
+              })}
+            </div>
+          )
+        },
+        ],
       columns: [
         {
           "title": "Номер заявки",
@@ -252,6 +307,34 @@ class Pulls extends Component {
     });
   }
 
+  deleteFile=(record, item)=>{
+    const { dispatch } = this.props;
+    dispatch({
+      type: "universal/deleteObject",
+      payload: {
+        "entity":"refundFile",
+        "alias":null,
+        "id": item.id
+      }
+    }).then(() => {
+      this.loadPull(this.state.pagingConfig.filter["refundPack.id"]);
+    });
+  }
+
+  uploadFile=(id, file)=>{
+        let formData = new FormData();
+        formData.append("content", file.file);
+        formData.append("entity", "Refund");
+        formData.append("path", "refundFiles");
+        formData.append("id", id);
+        request("/api/uicommand/uploadFile", {
+          method: "POST",
+          body: formData
+        }).then(() => {
+          this.loadPull(this.state.pagingConfig.filter["refundPack.id"]);
+        });
+  }
+  
   loadMainGridData = () => {
     /*const { dispatch } = this.props;
     dispatch({
